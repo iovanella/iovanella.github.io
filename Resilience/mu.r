fba39a3342c184bfc1b14fc7b99d806d1a6c8976
Antonio Iovanella <43239635+iovanella@users.noreply.github.com>
2018-12-20 16:25:00 +0100

    Update mu.r

--- Resilience/mu.r
+++ Resilience/mu.r
@@ -1,23 +1,23 @@
 mu <- function(G, delta, xi){
   
-  ## se il grafo è pesato poni in value i valori di weight, else poni a 1
+  ## if the network is weighted set the correct attribute, else set it at 1
   if(!is.null(E(G)$weight))
     G <- set_edge_attr(G, "value", value = E(G)$weight) else
       G <- set_edge_attr(G, "value", value = 1)
   
-  ## calcolo il diametro del grafo
+  ## network diameter
   diametro <- diameter(G)
   
-  ## calcolo tutti i simple path sul grafo
+  ## compute all the simple paths
   all_path <- list()
   for(i in 1:length(V(G)))
     all_path <- append(all_path, all_simple_paths(G, from = i, mode = "out"))
   max_path_lenght <- max(sapply(all_path, length)) - 1
   
-  ## calcolo la lunghezza del cammino più lungo
+  ## longest path
   max_path_lenght <- max(sapply(all_path, length)) - 1
   
-  ## calcolo i valori del cammino, dove il primo elemento della lista è i0 e ha epsilon0 == epsilon
+  ## compute the values for the path from i0
   xi_s <- vector(mode = 'list', length = length(all_path))
   for(i in 1:length(all_path)){
     appo <- xi
@@ -28,43 +28,43 @@ mu <- function(G, delta, xi){
       xi_s[[i]][j] <- (xi_s[[i]][j - 1] + xi_s[[i]][j]) * delta
   }
   
-  # calcolo tutti i cammini per le diverse lunghezze k
-  # ATTENZIONE: nella cella 1 ci sono il numero di cammini da i0 a i1
-  
+  # compute all the paths of lenght k
   cammini_totali <- matrix(0, 1, max_path_lenght)
   for(k in 1:max_path_lenght)
     cammini_totali[k] <- length(which(sapply(all_path, length) == k + 1))
   
-  # controllo se tutti i cammini sono stati considerati
+  # check if all paths were considered
   if(sum(cammini_totali) != length(all_path))
     print("ERRORE")
-  #primo modo di settare gamma
-  ## creo i vettori per la formula di mu
-  #gamma <- matrix(1, 1, max_path_lenght)
-  
-  #secondo modo di settare gamma  
+   
+  #first setting for gamma
   gamma <- matrix(1, 1, max_path_lenght)
-  appo <- 2 ^ (1:ceiling(length(gamma)/2))
-  gamma[(ceiling(length(gamma)/2) + 1):length(gamma)] <- appo[1:ceiling(length(gamma)/2)]
   
-  #terzo modo di settare gamma
+  #second setting for gamma  
+  #gamma <- matrix(1, 1, max_path_lenght)
+  #appo <- 2 ^ (1:ceiling(length(gamma)/2))
+  #gamma[(ceiling(length(gamma)/2) + 1):length(gamma)] <- appo[1:ceiling(length(gamma)/2)]
+  
+  #third setting for gamma
   #gamma <- matrix(1, 1, max_path_lenght)
   #appo <- 2 ^ (ceiling(length(gamma)/2):1)
   #gamma[1:(ceiling(length(gamma)/2))] <- appo[1:ceiling(length(gamma)/2)]
   
   #gamma <- 2 ^ ((max_path_lenght - 1):0) #sequenza geometrica
   
-  #primo modo di settare theta
+  #first setting for theta
   theta <- matrix(1/max_path_lenght, 1, max_path_lenght)
   
-  #secondo modo di settare theta
+  #second setting for theta
   #theta <- 1/(2 ^ (1:(max_path_lenght - 1)))
   #theta <- c(theta, theta[length(theta)])
   
-  #terzo modo di settare theta
+  #third setting for theta
+  #theta <- 1/(2 ^ (1:(max_path_lenght - 1)))
+  #theta <- c(theta, theta[length(theta)])
   #theta <- rev(theta)
   
-  #calcolo il valore di mu
+  #compute mu
   cammini_pc <- matrix(0, 1, max_path_lenght)
   for(k in 1:max_path_lenght){
     appo <- which(sapply(all_path, length) == k + 1)
